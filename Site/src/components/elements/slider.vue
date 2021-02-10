6f2fb8d8a86e4540e4ac182f109c459876242ade
jhaenen <jasper@haenenweb.nl>
2021-02-10 16:35:22 +0100

    Slider firefox implementation

---
+++ Site/src/components/elements/slider.vue
@@ -0,0 +1,68 @@
+<template>
+    <input ref="slider" class="slider" type="range" min="0" max="255" v-model="inValue"/>
+</template>
+
+<script>
+export default {
+    name: "sliderEl",
+    data() {
+        return {
+            inValue: 128,
+            color: "#ff00ff"
+        }
+    },
+    computed: {
+        value: function() {
+            return 255 - this.inValue;
+        }
+    },
+    methods: {
+        changeColor: function(color) {
+            this.$refs.slider.style.setProperty("--color", color)
+        }
+    }
+} 
+</script>
+
+<style lang="scss" scoped>
+    $borderCol: #ccd1d6;
+    $height: 44px;
+
+    .slider {
+        appearance: none;
+        height: $height;
+        border-radius: 8px;
+        border: 1px solid $borderCol;
+        width: 100%;
+        transform: rotate(180deg);
+
+        --color: #006CFF;
+    }
+
+    .slider:focus {
+        outline: none;
+    }
+
+    .slider::-moz-range-thumb {
+        border: 0.1px solid $borderCol;
+        height: $height;
+        width: 16px;
+        border-radius: 10px;
+        background: #ffffff;
+        cursor: pointer;
+    }
+
+    .slider::-moz-range-progress {
+        height: $height;
+        background: #ffffff;
+        border-radius: 8px 0px 0px 8px;
+        cursor: pointer;
+    }
+
+    .slider::-moz-range-track {
+        cursor: pointer;
+        height: $height;
+        background: linear-gradient(270deg, #ffffff, var(--color));
+        border-radius: 8px;
+    }
+</style>
--- Site/src/routes/lights.vue
+++ Site/src/routes/lights.vue
@@ -1,14 +1,67 @@
 <template>
-    <div>Verlichting</div>
+    <div class="center">
+        <div>
+            <div :class="{itemHolder: true, center: true, darkMode: SD.darkMode}">
+                <div :class="{items: true, darkMode: SD.darkMode}">
+                    <div class="item title">Lichtsterkte</div>
+                    <div class="item"><slider-el/></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import Store from "../stores/Store"
 
+import sliderEl from "../components/elements/slider.vue"
+
 export default {
     name: "Lights",
+    components: { sliderEl },
+    data() {
+        return {
+            SD: Store.data
+        }
+    },
     mounted() {
         Store.data.back = true;
     }
 };
 </script>
+
+<style lang="scss" scoped>
+    .itemHolder {
+        box-sizing: border-box;
+        margin: 20px 0;
+        max-width: 400px;
+        width: calc(100vw - 32px);
+        box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+        border-radius: 8px;
+        padding: 30px 24px 10px 24px;
+    }
+
+    .itemHolder.darkMode {
+        background: #1d2327;
+        box-shadow: 0 0px 0px rgba(0, 0, 0, 0);
+    }
+
+    .items {
+        width: 100%;
+    }
+
+    .items.darkMode {
+        background: #1d2327;
+        border-radius: 8px;
+    }
+
+    .item {
+        display: flex;
+        padding-bottom: 20px;
+    }
+
+    .title {
+        font-size: 18px;
+        font-weight: 600;
+    }
+</style>
